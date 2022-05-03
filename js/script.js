84966a12a91bd0e8c24383c1806caf02580410bf
BlueberryWeb <blueberryweb7@gmail.com>
2022-05-03 12:26:02 -0500

    parte web

--- js/script.js
+++ js/script.js
@@ -5,4 +5,11 @@
       target.classList.add('visible-card');
     }, false);
   });
+  document.querySelectorAll('.onboarding-card-web__arrow').forEach(function (arrow) {
+    var target = document.querySelector(arrow.dataset.target);
+    arrow.addEventListener('click', function (e) {
+      document.querySelector('.visible-card-web').classList.remove('visible-card-web');
+      target.classList.add('visible-card-web');
+    }, false);
+  });
   
